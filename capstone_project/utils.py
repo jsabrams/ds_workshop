6829cf5526162864feb591020343544dee04e411
Jared Abrams <jared.abrams@gmail.com>
2016-03-11 16:13:14 -0600

    added mean_bootstrap function

--- capstone_project/utils.py
+++ capstone_project/utils.py
@@ -30,3 +30,15 @@ def mean_diff_bootstrap(a, b, hi_ci, lo_ci, reps = 10000):
 	b_boot = [b[b.index[np.random.randint(0,len(b),len(b))]].mean() for i in range(reps)]
 	diff = np.array(a_boot) - np.array(b_boot)
 	return np.percentile(diff, [lo_ci, hi_ci])
+
+def mean_bootstrap(a, hi_ci, lo_ci, reps = 10000):
+	"""
+	Purpose: return a confidence interval on the mean of a
+	Inputs: a: pandas series a
+			hi_ci: the upper percentile of the distribution desired (0-100)
+			lo_ci: the lower percentile of the distribution desired (0-100)
+			reps: the number of bootstrap repetitions (default is 10,000)
+	Output: a list with the lower and uper bounds specified
+	"""
+	a_boot = [a[a.index[np.random.randint(0,len(a),len(a))]].mean() for i in range(reps)]
+	return np.percentile(a_boot, [lo_ci, hi_ci])
